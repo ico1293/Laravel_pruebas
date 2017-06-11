fb25a4ffec3741161f8139d84b8e5b054cf0fdf5
Stiven Trujillo <ico1293@gmail.com>
2017-06-11 14:33:14 -0500

    Creado el seeder, y modificado el factories

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,11 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Commentary::class, function (Faker\Generator $faker) {
+
+    return [
+        'name' => $faker->name,
+        'commentary' => $faker->realText(100),
+    ];
+});
